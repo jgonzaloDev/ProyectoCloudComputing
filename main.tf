ee42806f7003531ce598f354ea4774787f452fdc
jgonzaloDev <quinonesramirezjuniorgonzalo@gmail.com>
2026-05-09 14:14:35 -0500

    1

--- main.tf
+++ main.tf
@@ -5,13 +5,6 @@ terraform {
       version = "~> 3.0"
     }
   }
-
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateamorrescate"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
